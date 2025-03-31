481c97e36220bd7d538b70fa842864d5357a3650
James Zhu <james.zhu@fengheasia.com>
2025-03-31 18:04:34 +0800

    Support init submodules

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -11,9 +11,9 @@ TOKEN=$(python get-token.py)
 GITHUB_REPO_OWNER=${GITHUB_REPO_OWNER:-FinAI-Project}
 REPO_URL="https://x-access-token:$TOKEN@github.com/$GITHUB_REPO_OWNER/$GITHUB_REPO_NAME.git"
 if [ -n "$GITHUB_REF_NAME" ]; then
-    git clone --branch "$GITHUB_REF_NAME" "$REPO_URL" model
+    git clone --branch "$GITHUB_REF_NAME" --recurse-submodules "$REPO_URL" model
 else
-    git clone "$REPO_URL" model
+    git clone --recurse-submodules "$REPO_URL" model
 fi
 
 if [ -n "$WORK_DIR" ]; then
