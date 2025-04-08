e72a789064758b0bf8608f64bb65bfb1c5f21663
James Zhu <james.zhu@fengheasia.com>
2025-04-08 17:19:18 +0800

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-if [ -z "$GITHUB_REPO_NAME" ]; then
-    echo "Error: GITHUB_REPO_NAME is required."
+if [ -z "$GITHUB_REPO" ]; then
+    echo "Error: GITHUB_REPO is required."
     exit 1
 fi
 
 set -e
 
 TOKEN=$(python get-token.py)
-GITHUB_REPO_OWNER=${GITHUB_REPO_OWNER:-FinAI-Project}
-REPO_URL="https://x-access-token:$TOKEN@github.com/$GITHUB_REPO_OWNER/$GITHUB_REPO_NAME.git"
-if [ -n "$GITHUB_REF_NAME" ]; then
-    git clone --branch "$GITHUB_REF_NAME" --recurse-submodules "$REPO_URL"
+REPO_URL="https://x-access-token:$TOKEN@github.com/$GITHUB_REPO.git"
+if [ -n "$GITHUB_REF" ]; then
+    git clone --branch "$GITHUB_REF" --recurse-submodules "$REPO_URL"
 else
     git clone --recurse-submodules "$REPO_URL"
 fi
