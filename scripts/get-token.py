8f9009f93f6a3d3efe553d65cfe0565c0ee19e90
James Zhu <james.zhu@fengheasia.com>
2025-04-18 16:06:21 +0800

    Update requirements.txt

--- scripts/get-token.py
+++ scripts/get-token.py
@@ -8,9 +8,9 @@ import requests
 
 def generate_jwt(app_id, private_key):
     if not app_id:
-        raise ValueError("GITHUB_APP_ID is required.")
+        raise ValueError("github_app_id is required.")
     if not private_key:
-        raise ValueError("GITHUB_APP_PRIVATE_KEY is required.")
+        raise ValueError("github_app_private_key is required.")
     now = int(time.time())
     payload = {"iat": now, "exp": now + 600, "iss": app_id}
     token = jwt.encode(payload, private_key, algorithm="RS256")
@@ -21,7 +21,7 @@ def get_access_token(token, installation_id):
     if not token:
         raise ValueError("Failed to generate jwt.")
     if not installation_id:
-        raise ValueError("GITHUB_APP_INSTALLATION_ID is required.")
+        raise ValueError("github_app_installation_id is required.")
     headers = {"Authorization": f"Bearer {token}", "Accept": "application/vnd.github.v3+json"}
     url = f"https://api.github.com/app/installations/{installation_id}/access_tokens"
     response = requests.post(url, headers=headers, timeout=5)
@@ -32,8 +32,8 @@ def get_access_token(token, installation_id):
 
 if __name__ == "__main__":
     try:
-        jwt_token = generate_jwt(os.getenv("GITHUB_APP_ID"), os.getenv("GITHUB_APP_PRIVATE_KEY"))
-        access_token = get_access_token(jwt_token, os.getenv("GITHUB_APP_INSTALLATION_ID"))
+        jwt_token = generate_jwt(os.getenv("github_app_id"), os.getenv("github_app_private_key"))
+        access_token = get_access_token(jwt_token, os.getenv("github_app_installation_id"))
         print(access_token)
         sys.exit(0)
     except Exception as ex:  # pylint: disable=broad-exception-caught
