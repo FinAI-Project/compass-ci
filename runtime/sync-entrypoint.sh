055a0c04d1658eb1a2023587c32a9faf04b55019
James Zhu <james@fengheasia.com>
2025-06-13 17:48:57 +0800

    Fix rsync issues

--- runtime/sync-entrypoint.sh
+++ runtime/sync-entrypoint.sh
@@ -10,7 +10,7 @@ set +e
 while [ ! -f "$WORK_DIR/done" ]; do
     minute=$(date +'%-M')
     if [ $((minute % 3)) -eq 0 ]; then
-        rsync -rlt --delete --exclude=".*" "$WORK_DIR/" "$OUTPUT_DIR"
+        rsync -rlt --exclude=".*" "$WORK_DIR/" "$OUTPUT_DIR"
     fi
     sleep 60
 done
